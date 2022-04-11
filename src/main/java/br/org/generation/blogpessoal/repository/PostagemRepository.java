f91ddf69c34eb48fd7cbeea1237596f0fce82e6f
Kaua Mauricio <kauacariocaktd@gmail.com>
2022-04-11 15:13:18 -0300

    subindo projeto pessoal

---
+++ src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,22 @@
+package br.org.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.blogpessoal.model.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	/**
+	 * Method Query equivalente a instrução SQL: 
+	 * 
+	 * SELECT * FROM tb_postagem where titulo like "%titulo%";
+	 */
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+
+}
